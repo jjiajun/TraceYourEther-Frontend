b9492ccd52468d31ebd532c441e01a5206771b2d
DominiqueYeo <dominiqueyeo@hotmail.com>
2022-02-24 22:14:59 +0800

    updated to include friends dropdown list in makeRequest

--- src/components/MakeRequest.jsx
+++ src/components/MakeRequest.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import axios from "axios";
+import React, {useState,useContext} from "react";
+import { userContext } from "../context";
+import Message from "./Message"
+const {REACT_APP_BACKEND} = process.env
 
 export default function MakeRequest(friends) {
   console.log(friends)
+  const [amount,setAmount] = useState('');
+  const [description , setDescription] = useState('');
+
  
   if(!friends.friends) return <div />;
   if(!friends.friends[0]) {
@@ -11,14 +18,38 @@ export default function MakeRequest(friends) {
       </div>
     )
   }
+  const friendOptions = friends.friends.map((friend,index)=>(
+         <option key={index} value={`${friend._id}`}>{`${friend.name}`}</option>
+  ))
+  console.log('fri',friendOptions)
+
+    const requestAttempt = () => {
+
+    }
 
   return(
     <div>
       <select id='friends'>
-        {friends.map((friend)=>{
-          <option value={`${friend}`}>{`${friend}`}</option>
-        })}
+        {friendOptions}
       </select>
+      <input
+            name="amount"
+            id="amount"
+            placeholder="Amount"
+            onChange={(event) => setAmount(event.target.value)}
+          />
+      <input
+            name="description"
+            id="description"
+            placeholder="Description"
+            onChange={(event) => setDescription(event.target.value)}
+          />
+      <button className="btn sub-btn" type="submit" onClick={requestAttempt} >
+            Submit request
+            {' '}
+          </button>
+      {/* <Message displayMessage = {message} /> */}
+
     </div>
   )
 }
